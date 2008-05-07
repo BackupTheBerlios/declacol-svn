6b5752d4f837a9e7efea3478090f1bababa990a4
so9 <so9@6d69275e-2747-0410-805e-e47cb70a97ed>
2008-05-07 10:06:20 +0000

    Kompression kpl. aus Streams umgestellt

--- trunk/classes/class_tinyrle.pas
+++ trunk/classes/class_tinyrle.pas
@@ -29,162 +29,205 @@ type TTinyRLE = class(TObject)
      protected
      public
            function test(Size : unsigned32):boolean;
-           function compress  (input : longstring):longstring;
-           function uncompress(input : longstring):longstring;
+           procedure compress  (input,output : TStream);
+           procedure uncompress(input,output : TStream);
 end;
 
 
 implementation
 
-
+////////////////////////////////////////////////////////////////////////////////
+//Interner Test der Kompression
+////////////////////////////////////////////////////////////////////////////////
 function TTinyRLE.test(size : unsigned32):boolean;
 var
-   sTemp1   : longstring;
-   sTemp2   : longstring;
-   sTemp3   : longstring;
+   Temp1    : TMemoryStream;
+   Temp2    : TMemoryStream;
+   Temp3    : TMemoryStream;
+   u32Count : unsigned32;
+   u8Data   : unsigned8;
+   u8Data1  : unsigned8;
 begin
-     sTemp1:='';
+     result:=TRUE;
+
+     Temp1:=TMemoryStream.Create();
+     Temp2:=TMemoryStream.Create();
+     Temp3:=TMemoryStream.Create();
+
      while (Size > 0) do
            begin
-                sTemp1:=sTemp1 + chr(random(255));
-                dec (Size);
+                //Zufallsstring bauen
+                Temp1.Size:=0;
+                Temp1.Position:=0;
+                
+                randomize();
+                u32Count:=Size;
+                while (u32Count > 0) do
+                      begin
+                           u8Data:=random(255);
+                           u8Data:=65;
+                           Temp1.Write(u8Data,SizeOf(u8Data));
+                           dec (u32Count);
+                      end;
+
+                Temp1.Position:=0;
+                Temp2.Position:=0;
+                Temp2.Size:=0;
+                Temp3.Position:=0;
+                Temp3.Size:=0;
+
+                if (size=4) then
+                   begin
+                        result:=TRUE;
+                   end;
+
+                Self.Compress(Temp1,Temp2);
+                Self.UnCompress(Temp2,Temp3);
+
+                //Und nun Stream 1 und Stream 2 vergleichen
+                Temp1.Position:=0;
+                Temp3.Position:=0;
+
+
+                while (Temp1.position < Temp1.Size) do
+                      begin
+                           Temp1.Read(u8Data,SizeOf(u8Data));
+                           Temp3.Read(u8Data1,SizeOf(u8Data1));
+
+                           result:=result AND ( u8Data = u8Data1);
+                      end;
+
+                size:=size shr 1;
            end;
 
-     sTemp2:=Self.Compress(sTemp1);
-     sTemp3:=Self.UnCompress(sTemp2);
-
-     result:= sTemp1 = sTemp3;
+     Temp1.Free();
+     Temp2.Free();
+     Temp3.Free();
 end;
 
-function TTinyRLE.compress(input : longstring):longstring;
+////////////////////////////////////////////////////////////////////////////////
+/// Einfache RLE-Kompression durchführen
+////////////////////////////////////////////////////////////////////////////////
+//Dabei wird auf ein Escape zeichen verzichtet sondern
+//Das zu komprimierende Zeichen selbst als Escape benutzt.
+//
+//
+//Kommt ein Zeichen mehrfach vor so wir es zweimal ausgegeben, gefolgt vom
+//Multiplikator wir oft das Zeichen wiederholt wird. Kommt ein Zeichen nur
+//einfach vor wird es durchgereicht
+//aus
+//
+// aus aaabbcdegghhhhij wird aa#02bb#01cdefgg#01hh#03ij
+//
+
+procedure TTinyRLE.compress(input,output : TStream);
 var
    u8Counter  : unsigned8;
-   u32Index   : unsigned32;
-   u32Len     : unsigned32;
-   u8Last     : char;
+   u8Current  : unsigned8;
+   u8Last     : unsigned8;
 begin
-     result:='';
-
      u8Counter:=0;
-     u32Len:=length(input);
+     Input.Position:=0;
 
-     if (u32Len > 1) then
+     if (input.size > 1) then
         begin
              //Beim zweiten Zeichen anfangen
-             u32Index:=1;
-             u8Last:=input[u32Index];
-             inc(u32Index);
+             input.Read(u8Last,SizeOf(u8Last));
 
-             repeat
+             while (Input.Position < Input.Size) do
+                   begin
+                   input.read(u8Current,SizeOf(u8Current));
+
                    //Zeichenwiederholung ?
                    //Oder der Counter läuft über ?
-                   if (input[u32Index] = u8Last) AND (u8Counter < 255) then
+                   if (u8Current = u8Last) AND (u8Counter < 255) then
                       begin
                            inc(u8Counter);
                       end
                    else
                       begin
-                           //Nein, aber eine Sequenz vorrüber ?
+                           //Zeichen schreiben
+                           Output.Write(u8Last,SizeOf(u8Last));
+
+                           //Sequenz vorrüber ?
                            if (u8Counter > 0) then
                               begin
                                    //Ja => Multiplikator anhängen
-                                   //Beim Dekomprimieren müssen wir den Counter um eins runtersetzen.
-                                   //Hier lassen wir aber den Wert 1 um kein #0 in den Strom einzufügen
-                                   result:=result + u8Last + u8Last + Chr(u8Counter);
+                                   Output.Write(u8Last,SizeOf(u8Last));
+                                   Output.Write(u8Counter,SizeOf(u8Counter));
                                    u8Counter:=0;
-                              end
-                           else
-                              begin
-                                   //Nein => Zeichen anhängen
-                                   result:=result + u8Last;
                               end;
                       end;
 
                    //Für die nächste Runde merken
-                   u8Last:=input[u32Index];
-                   inc(u32Index);
-             until (u32Len < u32Index);
+                   u8Last:=u8Current;
+             end;
 
              //Der Rest
+             Output.Write(u8Last,SizeOf(u8Last));
              if (u8Counter > 0) then
                 begin
-                     //Ja => Multiplikator anhängen
-                     result:=result + u8Last + u8Last + Chr(u8Counter);
-                end
-             else
-                begin
-                     //Nein => Zeichen anhängen
-                     result:=result + u8Last;
+                     Output.Write(u8Last,SizeOf(u8Last));
+                     Output.Write(u8Counter,SizeOf(u8Current));
                 end;
         end
      else
         begin
              //String lohnt nicht zu komprimieren
-             result:=input;
+             Output.CopyFrom(Input,Input.Size);
         end; 
 
 end;
 
-function TTinyRLE.uncompress(input : longstring):longstring;
+////////////////////////////////////////////////////////////////////////////////
+/// Einfache RLE-Dekompression durchführen
+////////////////////////////////////////////////////////////////////////////////
+//Sind zwei Identische Zeichen hintereinander im Datenstrom wird das erste Zeichen
+//direkt ausgegeben, daß zweite Zeichen aber so oft wiederholt wie hinter dem
+//zweiten Zeichen angegeben ist. Kommt ein Zeichen nur einmal vor, wird es
+//einfach durchgereicht.
+procedure TTinyRLE.uncompress(input,output : TStream);
 var
-   u32Counter : unsigned32;
-   u32Index   : unsigned32;
-   u32Len     : unsigned32;
-   u8Last     : char;
+   u8Counter  : unsigned8;
+   u8Last     : unsigned8;
+   u8Current  : unsigned8;
+
 begin
-     result:='';
-     u32Len:=length(input);
-
      //Beim zweiten Zeichen anfangen
-     u32Index:=1;
-     u8Last:=input[u32Index];
-     inc(u32Index);
+     Input.Position:=0;
+     Input.Read(u8Current,SizeOf(u8Current));
 
-     if (u32Index <= u32Len) then
+     while (input.position < input.size) do
         begin
-             repeat
-                   //Kommen zwei identische Zeichen muß das nächste Zeichen ein
-                   //Multiplikator sein
-                   if (u8Last = input[u32Index]) then
-                      begin
-                           //Multiplikator lesen und String auffüllen
-                           inc(u32Index);
-                           //Überlauf abfangen
-                           if (u32Index <= u32Len) then
-                              begin
-                                   u32Counter:=Ord(input[u32Index]);
-                                   result:=result + u8Last + StringOfChar(u8Last,u32Counter);
-                              end;
-                              
-                           inc(u32Index);
-                      end
-                   else
-                      begin
-                           result:=result + u8Last;
-                      end;
+             //Für die nächste Runde merken
+             u8Last:=u8Current;
 
-                   //Überlauf abfangen
-                   if (u32Index <= u32Len) then
-                      begin
-                           u8Last:=input[u32Index];
-                      end;
+             //Erstes Zeichen können wir immer ausgeben
+             Output.Write(u8Last,SizeOf(u8Last));
 
-                      inc(u32Index);
+             //Kommen zwei identische Zeichen muß das nächste Zeichen ein
+             //Multiplikator sein
+             Input.Read(u8Current,SizeOf(u8Current));
+             if (u8Last = u8Current) then
+                begin
+                     //Multiplikator lesen und String auffüllen
+                     Input.Read(u8Counter,SizeOf(u8Counter));
+                     while (u8Counter > 0) do
+                           begin
+                                Output.Write(u8Last,SizeOf(u8Last));
+                                dec(u8Counter);
+                           end;
 
-             until (u32Index > u32Len);
+                     //Wieder mit dem Eingangsstrom synchronisieren
+                     Input.Read(u8Current,SizeOf(u8Current));
+                end;
+        end;
 
-             //Letztes Zeichen ?
-             if (u8Last = input[u32Len]) then
-                 begin
-                      result:=result + u8Last;
-                 end;
-        end
-     else
+     //Letztes Zeichen anhängen
+     if (u8Last <> u8Current) then
         begin
-             //String zu kurz
-             result:=input;
-        end; 
-
+             Output.Write(u8Current,SizeOf(u8Current));
+        end;
 end;
 
 
