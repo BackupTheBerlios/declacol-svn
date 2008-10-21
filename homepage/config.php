73acef0317cdcd3be2a6cf564f08fbce064fbb0b
so9 <so9@6d69275e-2747-0410-805e-e47cb70a97ed>
2008-10-21 11:48:41 +0000

    Added items remotely
    C:\DevWeb\labor\classes
    C:\DevWeb\labor\extern
    C:\DevWeb\labor\libs
    C:\DevWeb\labor\registry
    C:\DevWeb\labor\config.php
    C:\DevWeb\labor\index.php
    C:\DevWeb\labor\setup.php
    C:\DevWeb\labor\_cleanup.cmd

---
+++ homepage/config.php
@@ -0,0 +1,26 @@
+<script language="php">
+//Alle Pfade
+define ("PATH_BASE"    ,"./");
+define ("PATH_CLASSES" ,PATH_BASE."classes/");
+define ("PATH_LIBS"    ,PATH_BASE."libs/");
+define ("PATH_REGISTRY",PATH_BASE."registry/");
+define ("PATH_EXTERN"  ,PATH_BASE."extern/");
+define ("PATH_DATA"    ,PATH_BASE."files/");
+define ("PATH_IMAGES"  ,PATH_DATA."images/");
+define ("PATH_FILES"   ,PATH_DATA."files/");
+
+//Alles mit EMail
+define ("EMAIL_SMTP"   ,"mail.test.com");
+define ("EMAIL_POP3"   ,"mail.test.com");
+define ("EMAIL_USER"   ,"user");
+define ("EMAIL_PASS"   ,"pass");
+define ("EMAIL_FROM"   ,"check@test.com");
+define ("EMAIL_AUTH"   ,TRUE);
+define ("EMAIL_SUPPORT",TRUE);
+define ("EMAIL_MODE"   ,"smtp");
+
+
+//Allgemeine Definitionen
+define ("ID_NONE"      ,0);
+define ("SALT"         ,"876");
+</script>
