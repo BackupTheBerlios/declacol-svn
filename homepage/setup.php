e89a1fc5652ea52cce6724e4df287be53c74b7e2
so9 <so9@6d69275e-2747-0410-805e-e47cb70a97ed>
2008-10-21 11:48:51 +0000

    Added items remotely
    C:\DevWeb\labor\classes
    C:\DevWeb\labor\extern
    C:\DevWeb\labor\libs
    C:\DevWeb\labor\registry
    C:\DevWeb\labor\config.php
    C:\DevWeb\labor\index.php
    C:\DevWeb\labor\setup.php
    C:\DevWeb\labor\_cleanup.cmd

---
+++ homepage/setup.php
@@ -0,0 +1,23 @@
+<pre>
+<script language="php">
+//Nur diese Basisconfigurationen ziehen,
+//der Rest liegt in den Reg-Files
+require_once("config.php");
+require_once(PATH_CLASSES."class.registry.php");
+require_once(PATH_CLASSES."class.classinst.php");
+require_once(PATH_CLASSES."class.classload.php");
+
+$inst=new classinst(PATH_REGISTRY);
+$inst->registerall(PATH_CLASSES);
+$inst->destroy();
+
+$loader=new classload(PATH_REGISTRY);
+$loader->load();
+
+print_r($CLASSES);
+
+
+
+
+$loader->destroy();
+</script>
