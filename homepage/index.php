25b030163147928a383f859f28e42b3a09ef43d0
so9 <so9@6d69275e-2747-0410-805e-e47cb70a97ed>
2008-10-28 08:57:02 +0000

    funktionsseite nach "innen" verlagert

--- homepage/index.php
+++ homepage/index.php
@@ -19,47 +19,27 @@ $classloader=new classload(PATH_REGISTRY);
 $classloader->load();
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-require_once(PATH_CLASSES."class.rpc.php");
-$rpc=new rpc();
-$rpc->userstatus=ID_NONE;
-$paramarray=array("modul"  =>"Hauptprojekt",
-                  "title"  =>"Mein Neuer Eintrag",
-                  "link"   =>"http://www.google.de",
-                  "message"=>"Nur eine kleine Nachricht",
-                  "author" =>"Sven Lorenz");
-
-$temp = $rpc->create ("vfbtygvsljcandhb",rand(0,65535),"newsletter","add",$paramarray);
-$rpc->process($temp);
-
-
-
-//Templateengine einhängen
-$pagefile =strtolower(classcall("request","getrequest","page","news",FILTER_ALPHANUM));
-$template ="main.txt";
-$id=$template.$pagefile;
-
-setproperty("unimatrix","cacheengine" ,$CLASSES["cache"]);
-setproperty("unimatrix","cachetimeout",300);
-//Seite nur erzeugen, wenn sie nicht gepuffert ist
-if (classcall("unimatrix","iscached",$id) == FALSE)
+//Nun einfach die Funktion auswählen
+if ( classcall("request","getrequest","rpc",FALSE,FILTER_SECURE) != FALSE)
     {
-    classcall("unimatrix","assign","pagetitle",$pagefile);
-    classcall("unimatrix","assign","sitename","Guru-Meditation");
-    classcall("unimatrix","assign","pagefile",$pagefile.".txt");
-    classcall("unimatrix","assign","version","1.0");
-    classcall("unimatrix","assign","user","Sven Lorenz");
-    classcall("unimatrix","assign","login",TRUE);
-    classcall("unimatrix","assign","menu",array("?page=news"       => "NEWS",
-                                                "?page=impressum"  => "IMPRESSUM",
-                                                "?page=links"      => "LINKS",
-                                                "?page=faq"        => "FAQ",
-                                          ));
+    //Ein Remotecall ?
+    include(PATH_BASE."rpc.php");
     }
-    
-//Here we go
-echo classcall("unimatrix","render",$id,$template);
+else
+    {
+    //Ein Download ?
+    if ( classcall("request","getrequest","file",FALSE,FILTER_SECURE) != FALSE)
+        {
+        include(PATH_BASE."push.php");
+        }
+    else
+        {
+        //Alles andere
+        include(PATH_BASE."index.php");
+        }
+        
 
-
+    }
 //Alle Klassen entladen und Inhalte flushen
 $classloader->destroy();
 </script>
