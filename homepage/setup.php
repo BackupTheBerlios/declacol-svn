07d6661a7e2faa563424e21096c36fdaafe06dd0
so9 <so9@6d69275e-2747-0410-805e-e47cb70a97ed>
2008-11-18 15:07:25 +0000

    Erste Schritte des Setups gemacht. Unimatrix muß noch auf rekursion umgearbeitet werden

--- homepage/setup.php
+++ homepage/setup.php
@@ -2,90 +2,79 @@
 define("SETUP",TRUE);
 //Nur diese Basisconfigurationen ziehen,
 //der Rest liegt in den Reg-Files
-require_once("./config/config.php");
-require_once(PATH_CLASSES."class.registry.php");
-require_once(PATH_CLASSES."class.classinst.php");
-require_once(PATH_CLASSES."class.classload.php");
-require_once(PATH_CLASSES."class.configurator.php");
+require_once("./setup/class.unimatrix.php");
+require_once("./setup/class.configurator.php");
 
-//Alle Klassen initialisieren
-$inst=new classinst(PATH_REGISTRY);
-$inst->registerall(PATH_CLASSES);
-$inst->destroy();
+$render=new unimatrix();
+$render->basepath="./setup/templates/";
 
-//Und direkt laden
-$loader=new classload(PATH_REGISTRY);
-$loader->load();
-
-//Setupschritt holen
-$step = callmethod("request","getrequest","page",1,FILTER_NUMBER);;
-
-
-
-
-//Die Installationsschritte auswählen
-switch ($step)
+if (checkdata()!=TRUE)
     {
-    case 2  : getmaildata($step);   break;
-    case 3  : getrootdata($step);   break;
-    case 4  : transferfiles($step); break;
-    case 5  : done();               break;
-    default : welcome(1);           break;
+    welcome();
+    }
+else
+    {
+    transferfiles();
     }
 
-//Einfach die Templateengine nutzen
-callmethod("unimatrix","assign","step"    ,intval($step));
-callmethod("unimatrix","assign","nextstep",intval($step+1));
-echo callmethod("unimatrix","render","egal","setup_main.txt");
 
-$loader->destroy();
+$render->destroy();
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 /// Ab hier nur noch Funktionen
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Wilkommen
-function welcome($step)
+function welcome()
     {
+    global $render;
+    
     //Evtl. Schreibrechte anfordern
     $access=is_writable("setup.php") && is_writable("./config/local.config.php");
-    callmethod("unimatrix","assign","access"  ,$access);
-    callmethod("unimatrix","assign","noaccess",!$access);
-    callmethod("unimatrix","assign","setup" ,!is_writable("setup.php"));
-    callmethod("unimatrix","assign","config",!is_writable("./config/local.config.php"));
+    $render->assign("access"  ,$access);
+    $render->assign("noaccess",!$access);
 
-    //Pluginseite einhängen
-    callmethod("unimatrix","assign","pagefile","setup_welcome.txt");
+    //Dateirechte
+    $render->assign("setup" ,!is_writable("setup.php"));
+    $render->assign("config",!is_writable("./config/local.config.php"));
+
+    //Ausgabe
+    $render->assign("pagefile","setup_welcome.txt");
+    echo $render->render("egal","setup_main.txt");
     }
     
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Die angepasste Installation in das Zielverzeichnis übertragen
+function transferfiles()
+    {
+    global $render;
+
+    //Ausgabe
+    $render->assign("pagefile","setup_working.txt");
+    echo $render->render("egal","setup_main.txt");
+    }
+    
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Fertig
 function done()
     {
-    //Pluginseite einhängen
-    callmethod("unimatrix","assign","pagefile","setup_done.txt");
+    global $render;
+
+    //Ausgabe
+    $render->assign("pagefile","setup_working.txt");
+    echo $render->render("egal","setup_main.txt");
     }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Aller Mailerdaten abfragen
-function getmaildata($step)
+function checkdata()
     {
-    //Pluginseite einhängen
-    callmethod("unimatrix","assign","pagefile","setup_mailer.txt");
+    $result=FALSE;
+    if (isset($_POST["data0"])==TRUE)
+        {
+        if ( $_POST["data0"]!="" )
+            {
+            $result=TRUE;
+            }
+        }
+    return($result);
     }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Den Root-User anlegen
-function getrootdata($step)
-    {
-    //Pluginseite einhängen
-    callmethod("unimatrix","assign","pagefile","setup_rootuser.txt");
-    }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Die angepasste Installation in das Zielverzeichnis übertragen
-function transferfiles($step)
-    {
-    //Pluginseite einhängen
-    callmethod("unimatrix","assign","pagefile","setup_transfer.txt");
-    }
-
 </script>
