23aefea287de8c0374238295b30e5b2a16dd2ed7
so9 <so9@6d69275e-2747-0410-805e-e47cb70a97ed>
2009-05-10 19:28:43 +0000

    error in search function fixed

--- trunk/libraries/unit_hex.pas
+++ trunk/libraries/unit_hex.pas
@@ -34,11 +34,71 @@ uses unit_typedefs,unit_filesystem,sysutils,windows;
 //Zurückgegeben wir die Anzahl der Ersetzungen
 function searchandreplace(var data : array of byte; search : array of byte; replace : array of byte):unsigned32;
 
+function search(var data : array of byte; search : array of byte; var found : unsigned32):boolean;
+
 function hextofile(filename : longstring; data : pointer; size : unsigned32):boolean;
 
 implementation
 
+//Nach eine Bytefolge suchen
+function search(var data : array of byte; search : array of byte; var found : unsigned32):boolean;
+var
+  u32SLen   : unsigned32;
+  u32HLen   : unsigned32;
 
+  u32HIndex : unsigned32;
+  u32SIndex : unsigned32;
+
+  u32Temp   : unsigned32;
+begin
+  result:=FALSE;
+
+  u32SLen:=unsigned32(Length(search));
+  u32HLen:=unsigned32(Length(data));
+
+  //Längenfeler abfangen
+  if (u32HLen > 0) and (u32SLen > 0) then
+    begin
+      u32HIndex:=0;
+
+      //Eins abziehen da wir das Offset brauchen
+      dec(u32SLen);
+
+      //Alle Zeichen durchgehen
+      while (u32HIndex < (u32HLen - u32SLen) ) and (result = FALSE)  do
+        begin
+          //Stimmt das erste Zeichen?
+          u32SIndex:=0;
+          if (data[u32HIndex] = search[u32SIndex]) then
+            begin
+              //Nun vergleichen wir von hinten, ob die Zeichen gleich sind
+              //haben wir einen Unterschied, können wir abbrechen und den
+              //Vergleich von dieser Position neu starten
+              u32Temp:=u32SLen;
+              while (u32Temp > 0) and
+                    (data[u32HIndex + u32Temp] = search[u32Temp]) do
+                begin
+                  dec(u32Temp);
+                end;
+
+              //Ist u32Temp=0 ist die ganze Sequenz gleich
+              if (u32Temp=0) then
+                begin
+                  found :=u32HIndex;
+                  result:=TRUE;
+                end
+              else
+                begin
+                  //Nicht gefunden, dann überspringen
+                  inc(u32HIndex,u32SLen);
+                  inc(u32HIndex);
+                end;
+            end;
+          //Ganz normal weitersuchen
+          inc(u32HIndex);
+        end;
+    end;
+end;
 
 function searchandreplace(var data : array of byte; search : array of byte; replace : array of byte):unsigned32;
 var
