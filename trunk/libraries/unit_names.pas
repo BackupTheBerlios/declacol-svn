7894ee04a329b3518d7cf20d34193f27d17ae7f6
so9 <so9@6d69275e-2747-0410-805e-e47cb70a97ed>
2011-09-18 14:42:50 +0000

    added japanese names

--- trunk/libraries/unit_names.pas
+++ trunk/libraries/unit_names.pas
@@ -33,12 +33,24 @@ interface
 
 uses unit_typedefs,sysutils;
 
-function getname( randomized : boolean = FALSE ):longstring;
+const
+  NAMETYPE_DEFAULT  = 0;
+  NAMETYPE_DB       = 1;
+  NAMETYPE_JAPANESE = 2;
+
+
+function getname(nametype : unsigned32 = NAMETYPE_DEFAULT; randomized : boolean = FALSE ):longstring;
 
 implementation
 
+function stringtonumber(input : string):unsigned32; forward;
+function internalrand(seed : unsigned32):unsigned32; forward;
+
+function getdbname(seed:unsigned32):longstring; forward;
+function getjapanesename(seed:unsigned32):longstring; forward;
+
 var
-  names : array[0..44] of longstring =
+  namedb : array[0..44] of longstring =
   (
   'SilentBob','Wintermute','ScreamingFist','Deepthought','Mandelbrot',
   'EvilAdmin','Marvin','Beeblebrox','Gandalf','Rumo',
@@ -51,40 +63,100 @@ var
   'Uplink','Romeo','Egghead','Ghost','Case'
   );
 
-const
-  NAMECOUNT = length(names);
+  jap_sil : array[0..43] of longstring =
+  (
+  'a','e','i','o','u',
+  'ka','ki','ku','ke','ko',
+  'sa','shi','su','se','so',
+  'ta','chi','tsu','te','to',
+  'na','ni','nu','ne','no',
+  'ha','hi','fu','he','ho',
+  'ma','mi','mu','me','mo',
+  'ya','yu','yo',
+  'ra','ri','ru','re','ro',
+  'wa'
+  );
 
-/////////////////////////////////////////////////////////////////////////////////
-//convert a string into some kind of unique number
-/////////////////////////////////////////////////////////////////////////////////
-function stringtonumber(input : string; max : unsigned32):unsigned32;
-var
-  u32number : unsigned32;
-  u32index  : unsigned32;
-begin
-  u32number:=length(input);
-  u32index:=1;
-  while (u32index <= unsigned32(length(input))) do
-    begin
-      inc(u32number,u32index * ord(input[u32index]));
-      inc(u32index);
-    end;
-  result:=u32number mod max;
-end;
+const
+  NAMEDB_COUNT  = length(namedb);
+  JAPAN_COUNT   = length(jap_sil);
 
 /////////////////////////////////////////////////////////////////////////////////
 //exported function
 /////////////////////////////////////////////////////////////////////////////////
-function getname( randomized : boolean = FALSE ):longstring;
+function getname(nametype : unsigned32 = NAMETYPE_DEFAULT; randomized : boolean = FALSE ):longstring;
+var
+  u32seed : unsigned32;
 begin
   if (randomized = FALSE) then
     begin
-      result:=names[stringtonumber(extractfilename(paramstr(0)),NAMECOUNT-1)];
+      u32seed:=stringtonumber(extractfilename(paramstr(0)));
     end
   else
     begin
       randomize();
-      result:=names[random(NAMECOUNT)];
+      u32seed:=random(high(signed32));
+    end;
+
+  case (nametype) of
+    NAMETYPE_DB       : result:=getdbname(u32seed);
+    NAMETYPE_JAPANESE : result:=getjapanesename(u32seed);
+  else
+    //default
+    result:=getdbname(u32seed);
+  end;
+end;
+
+
+/////////////////////////////////////////////////////////////////////////////////
+// an internal random generator
+/////////////////////////////////////////////////////////////////////////////////
+function internalrand(seed : unsigned32):unsigned32;
+begin
+  result:=134775813 * seed + 1;
+end;
+
+/////////////////////////////////////////////////////////////////////////////////
+//convert a string into some kind of unique number
+/////////////////////////////////////////////////////////////////////////////////
+function stringtonumber(input : string):unsigned32;
+var
+  u32index  : unsigned32;
+begin
+  result:=length(input);
+  u32index:=1;
+  while (u32index <= unsigned32(length(input))) do
+    begin
+      inc(result,u32index * ord(input[u32index]));
+      inc(u32index);
+    end;
+end;
+
+/////////////////////////////////////////////////////////////////////////////////
+//get a name from the database
+/////////////////////////////////////////////////////////////////////////////////
+function getdbname(seed:unsigned32):longstring;
+begin
+  result:=namedb[seed mod (NAMEDB_COUNT - 1) ];
+end;
+
+/////////////////////////////////////////////////////////////////////////////////
+//create a pseudo-japanese name
+/////////////////////////////////////////////////////////////////////////////////
+function getjapanesename(seed:unsigned32):longstring;
+var
+  u32size : unsigned32;
+begin
+  //create random size
+  u32size:=seed mod 5;
+  if (u32size < 2) then u32size:=3;
+
+  result:='';
+  while (u32size > 0) do
+    begin
+      seed:=internalrand(seed);
+      result:=result +  jap_sil[seed mod (JAPAN_COUNT - 1)];
+      dec(u32size);
     end;
 end;
 
